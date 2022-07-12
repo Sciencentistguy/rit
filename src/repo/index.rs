aae91ce99663a783668c52599c531aff72f0f42f
Jamie Quigley <jamie@quigley.xyz>
2022-07-12 18:40:28 +0100

    "working" ser/de of index. Cannot handle extensions, gets very confused

--- src/repo/index.rs
+++ src/repo/index.rs
@@ -104,6 +104,7 @@ impl IndexEntry<'_> {
 struct Index<'a> {
     header: IndexHeader,
     entries: Vec<IndexEntry<'a>>,
+    oid: Digest,
 }
 
 fn parse_index(bytes: &[u8]) -> Index {
@@ -114,8 +115,17 @@ fn parse_index(bytes: &[u8]) -> Index {
         header.magic
     );
 
+    assert_eq!(
+        header.version, 2,
+        "Only git index version 2 is supported (this is version {})",
+        header.version
+    );
+
+    println!("reading {} entries", header.num_entries);
+
     let mut entries = Vec::new();
-    let mut offset = 12;
+    let bytes = &bytes[12..];
+    let mut offset = 0;
     for _ in 0..header.num_entries {
         let ctime_s = u32::from_be_bytes(bytes[offset..offset + 4].try_into().unwrap());
         offset += 4;
@@ -151,9 +161,14 @@ fn parse_index(bytes: &[u8]) -> Index {
         let name = {
             let len = unsafe { libc::strlen(bytes.as_ptr().add(offset).cast()) } + 1;
             let slc = &bytes[offset..offset + len];
-            offset += len + (len % 8);
+            offset += len;
             CStr::from_bytes_with_nul(slc).unwrap()
         };
+
+        // Git pads the end of the name with NUL bytes to align the next entry to multiples of 8
+        offset = align_to_8(offset);
+        // println!("{:x}", offset);
+
         entries.push(IndexEntry {
             ctime_s,
             ctime_n,
@@ -170,8 +185,13 @@ fn parse_index(bytes: &[u8]) -> Index {
             name,
         });
     }
+    let oid = Digest(bytes[offset..offset + 20].try_into().unwrap());
 
-    Index { header, entries }
+    Index {
+        header,
+        entries,
+        oid,
+    }
 }
 
 fn parse_index_header(bytes: &[u8; 12]) -> IndexHeader {
@@ -185,6 +205,85 @@ fn parse_index_header(bytes: &[u8; 12]) -> IndexHeader {
     }
 }
 
+fn align_to_8(num: usize) -> usize {
+    let extra = (num % 8);
+    match extra {
+        0 => num,
+        extra => {
+            let padsize = 8 - extra;
+            num + padsize
+        }
+    }
+}
+
+fn write_index(index: &Index) -> Vec<u8> {
+    let mut siz = std::mem::size_of::<IndexHeader>();
+    for entry in &index.entries {
+        siz += std::mem::size_of::<IndexEntry>() + entry.name.to_bytes().len();
+    }
+    let mut out = Vec::new();
+    write_index_header(&index.header, &mut out);
+
+    for entry in &index.entries {
+        write_index_entry(entry, &mut out);
+    }
+
+    let oid = Digest::new(&out);
+    // assert_eq!(oid, index.oid);
+
+    out.extend_from_slice(&oid.0);
+
+    out
+}
+
+fn write_index_header(hdr: &IndexHeader, dest: &mut Vec<u8>) {
+    dest.extend_from_slice(&hdr.magic);
+    dest.extend_from_slice(&hdr.version.to_be_bytes());
+    dest.extend_from_slice(&hdr.num_entries.to_be_bytes());
+}
+
+fn write_index_entry(
+    IndexEntry {
+        ctime_s,
+        ctime_n,
+        mtime_s,
+        mtime_n,
+        dev,
+        ino,
+        mode,
+        uid,
+        gid,
+        siz,
+        oid,
+        flags,
+        name,
+    }: &IndexEntry,
+    out: &mut Vec<u8>,
+) {
+    let start_len = out.len();
+    out.extend_from_slice(&ctime_s.to_be_bytes());
+    out.extend_from_slice(&ctime_n.to_be_bytes());
+    out.extend_from_slice(&mtime_s.to_be_bytes());
+    out.extend_from_slice(&mtime_n.to_be_bytes());
+    out.extend_from_slice(&dev.to_be_bytes());
+    out.extend_from_slice(&ino.to_be_bytes());
+    out.extend_from_slice(&mode.to_be_bytes());
+    out.extend_from_slice(&uid.to_be_bytes());
+    out.extend_from_slice(&gid.to_be_bytes());
+    out.extend_from_slice(&siz.to_be_bytes());
+    out.extend_from_slice(&oid.0);
+    out.extend_from_slice(&flags.to_be_bytes());
+    out.extend_from_slice(name.to_bytes_with_nul());
+    let len = out.len() - start_len;
+    let extra = len % 8;
+    if extra != 0 {
+        let padsize = 8 - extra;
+        for _ in 0..padsize {
+            out.push(b'\0');
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -192,10 +291,23 @@ mod tests {
     #[test]
     fn works() {
         let bytes = std::fs::read("/dev/shm/rit/.git/index").unwrap();
+        // let bytes = std::fs::read("/home/jamie/Git/susbot/.git/index").unwrap();
         let idx = parse_index(&bytes);
-        println!("{:?}", idx.entries[0].ctime_s);
-        println!("{:?}", idx.entries[0].name);
-        let s = idx.entries[1].name.to_str().unwrap();
-        println!("{} ({:?})", s, idx.entries[1].name);
+
+        for e in &idx.entries {
+            let name = e.name.to_str();
+            println!("{:?}", name);
+        }
+
+        let new_bytes = write_index(&idx);
+        assert_eq!(bytes, new_bytes);
+    }
+
+    #[test]
+    fn test_align_to_8() {
+        for i in 1..9 {
+            assert_eq!(align_to_8(i), 8);
+        }
+        assert!((0..64).map(align_to_8).all(|x| x % 8 == 0));
     }
 }
